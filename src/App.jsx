132dc9af1f4708d0b1f22fc548b3feb6fe2dd42e
HellenCristina <hellencrissaraujo28@gmail.com>
2025-05-26 10:34:12 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import background from './assets/background.jpg'
+import image from './assets/imagem.jpg'
+
+function App() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
